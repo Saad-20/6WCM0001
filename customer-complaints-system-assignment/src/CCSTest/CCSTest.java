d0c5bc22199621634d7f04e25b30a9847d3ab911
Chris Pritchard <chris@christopherpritchard.co.uk>
2017-11-09 01:06:42 +0000

    Continue work on CCS

--- customer-complaints-system-assignment/src/CCSTest/CCSTest.java
+++ customer-complaints-system-assignment/src/CCSTest/CCSTest.java
@@ -2,18 +2,17 @@ package CCSTest;
 import CustomerComplaintsSystemAssignment.*;
 import java.util.List;
 
-
 public class CCSTest {
 
     public static void main(String[] args){
         CCSImplementation ccs = new CCSImplementation();
         CCSGenerator ccsGenerator = new CCSGenerator(ccs);
-        ccsGenerator.generateCustomer(20);
-        ccsGenerator.generateStaff(10);
-        ccsGenerator.generateStaffComplaint(2);
-        ccsGenerator.generateLiftComplaint(3);
-        ccsGenerator.generateComment(3);
-        ccsGenerator.generateGeneralComplaint(5);
+        ccsGenerator.generateCustomer(200);
+        ccsGenerator.generateStaff(100);
+        ccsGenerator.generateStaffComplaint(20);
+        ccsGenerator.generateLiftComplaint(30);
+        ccsGenerator.generateComment(27);
+        ccsGenerator.generateGeneralComplaint(15);
         ccs.recordComplaintResolved(RandomGenerators.getRandomComplaintId(ccs));
         List<Customer> customers = ccs.getCustomerList();
         List<Staff> staff = ccs.getStaffList();
